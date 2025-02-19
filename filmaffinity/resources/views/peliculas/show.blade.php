1b0f58471db3e61b1f2523279f6bb7ba32a275ac
Alejandro Galán <alejandrogalancaballero@hotmail.com>
2025-02-19 21:09:31 +0100

    Crear ruta nueva para crear un comentario

--- filmaffinity/resources/views/peliculas/show.blade.php
+++ filmaffinity/resources/views/peliculas/show.blade.php
@@ -96,7 +96,7 @@
     @endforeach
 
     <div class="mt-6 text-center">
-        <form method="POST" action="{{ route('comentarios.store', $pelicula) }}" class="max-w-sm mx-auto">
+        <form method="POST" action="{{ route('comentarios.crear', $pelicula) }}" class="max-w-sm mx-auto">
             @csrf
             <div class="mb-5">
                 <textarea id="texto" name="texto" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Comentario..."></textarea>
